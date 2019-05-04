7f212bae3a8999c07f9c60c04a74cd13d14e4f02
Xingze Zhang <diggzhang@gmail.com>
2019-05-04 11:41:02 +0800

    Update best_university_ranking_spider.py
    Add comment

--- best_university_ranking_spider.py
+++ best_university_ranking_spider.py
@@ -22,6 +22,9 @@ class RequestWebPageUtil:
         print("[*] Current target url:", self.url)
 
     def requests_url(self):
+        """
+        :return: self.web_content - 由requests get到的页面html内容
+        """
         result = requests.get(self.url)
         result.encoding = 'utf-8'
         self.web_content = str(result.text)
@@ -30,6 +33,11 @@ class RequestWebPageUtil:
         print(self.web_content)
 
     def extract_table(self, selector_path):
+        """
+        从页面中提取表格部分的html
+        :param selector_path: 默认的path是全局变量中提取
+        :return: tables, 页面中的表格标签html
+        """
         _selector_path = selector_path
         if _selector_path is None or len(_selector_path) <= 1:
             """
@@ -43,18 +51,36 @@ class RequestWebPageUtil:
 
 
 def best_university_ranking_specialized_processor(tables, result_file):
+    """
+    针对最好大学页面的特定处理，删除无用列，然后存档到excel文件
+    :param tables: web_content.extract_table中从页面中提取的表格部分
+    :param result_file: 目标存储文件
+    :return: Nothing
+    """
     table_df = pd.read_html(str(tables[0]), index_col=[0, 1, 2, 3])
     del table_df[0]['指标得分 生源质量（新生高考成绩得分） 培养结果（毕业生就业率） 社会声誉（社会捐赠收入·千元） 科研规模（论文数量·篇） 科研质量（论文质量·FWCI） 顶尖成果（高被引论文·篇） 顶尖人才（高被引学者·人） 科技服务（企业科研经费·千元） 成果转化（技术转让收入·千元） 学生国际化（留学生比例）']
     table_df[0].to_excel(result_file)
 
 
 def extract_url(target_url):
+    """
+    将传入的target_url用RequestWebPageUtil类中封装的request方法请求并返回页面内容
+    :param target_url: 一个有效的URL地址，默认是全局变量中的TARGET_URL
+    :return: 返回target_url的页面内容html
+    """
     web_content = RequestWebPageUtil(target_url)
     web_content.requests_url()
     return web_content
 
 
 def best_university_ranking(web_content, selector_path, result_file):
+    """
+    解析最好大学页面中的表格，提取目标字段列，存储到result_file中
+    :param web_content: 页面内容html,由extract_url方法返回
+    :param selector_path: 默认selector是提取页面中的表格SELECTOR_PATH
+    :param result_file: 结构存储的文件地址
+    :return: Nothing
+    """
     best_university_ranking_2019_table = web_content.extract_table(selector_path)
     best_university_ranking_specialized_processor(best_university_ranking_2019_table, result_file)
 
